33f848a926766e5bc4ef61c7d9e0b8e9ed0112ee
Nick Wellnhofer <wellnhofer@aevum.de>
2025-01-02 00:14:27 +0100

    xpath: Don't sort in xmlXPathCastNodeSetToString

--- xpath.c
+++ xpath.c
@@ -4749,8 +4749,6 @@ xmlXPathCastNodeSetToString (xmlNodeSetPtr ns) {
     if ((ns == NULL) || (ns->nodeNr == 0) || (ns->nodeTab == NULL))
 	return(xmlStrdup((const xmlChar *) ""));
 
-    if (ns->nodeNr > 1)
-	xmlXPathNodeSetSort(ns);
     return(xmlXPathCastNodeToString(ns->nodeTab[0]));
 }
 
