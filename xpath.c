fc0a11e8c6fd6023ca1597f839254159ae270fa0
Nick Wellnhofer <wellnhofer@aevum.de>
2025-01-02 17:00:42 +0100

    xpath: Rework MergeAndClear

--- xpath.c
+++ xpath.c
@@ -3203,46 +3203,6 @@ error:
     return(NULL);
 }
 
-static int
-xmlXPathNodeSetMergeFirstLast(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
-                              xmlXPathEvalMode mode) {
-    int swap, index1, index2;
-    xmlNodePtr node1, node2;
-
-    /* assert(set1->nodeNr > 0 && set2->nodeNr > 0); */
-
-    /*
-     * Compare and swap first or last elements of node sets
-     */
-
-    if (mode == XPATH_EVAL_FIRST) {
-        index1 = 0;
-        index2 = 0;
-    } else {
-        index1 = set1->nodeNr - 1;
-        index2 = set2->nodeNr - 1;
-    }
-
-    node1 = set1->nodeTab[index1];
-    node2 = set2->nodeTab[index2];
-    swap = ((mode == XPATH_EVAL_FIRST) ? -1 : 1);
-
-    if (
-#ifdef XP_OPTIMIZED_NON_ELEM_COMPARISON
-         (xmlXPathCmpNodesExt(node1, node2))
-#else
-         (xmlXPathCmpNodes(node1, node2))
-#endif
-         == swap) {
-        set1->nodeTab[index1] = node2;
-        set2->nodeTab[index2] = node1;
-    }
-
-    xmlXPathNodeSetClear(set2, 1);
-
-    return(0);
-}
-
 /**
  * xmlXPathNodeSetMergeAndClear:
  * @set1:  the first NodeSet or NULL
@@ -3257,10 +3217,41 @@ xmlXPathNodeSetMergeFirstLast(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
  */
 static xmlNodeSetPtr
 xmlXPathNodeSetMergeAndClear(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
-                             xmlXPathEvalMode mode) {
+                             xmlXPathEvalMode mode, int checkDupls) {
     if ((mode == XPATH_EVAL_FIRST) || (mode == XPATH_EVAL_LAST)) {
-        if (xmlXPathNodeSetMergeFirstLast(set1, set2, mode) < 0)
-            goto error;
+        int swap, index1, index2;
+        xmlNodePtr node1, node2;
+
+        /* assert(set1->nodeNr > 0 && set2->nodeNr > 0); */
+
+        /*
+         * Compare and swap first or last elements of node sets
+         */
+
+        if (mode == XPATH_EVAL_FIRST) {
+            index1 = 0;
+            index2 = 0;
+        } else {
+            index1 = set1->nodeNr - 1;
+            index2 = set2->nodeNr - 1;
+        }
+
+        node1 = set1->nodeTab[index1];
+        node2 = set2->nodeTab[index2];
+        swap = ((mode == XPATH_EVAL_FIRST) ? -1 : 1);
+
+        if (
+#ifdef XP_OPTIMIZED_NON_ELEM_COMPARISON
+             (xmlXPathCmpNodesExt(node1, node2))
+#else
+             (xmlXPathCmpNodes(node1, node2))
+#endif
+             == swap) {
+            set1->nodeTab[index1] = node2;
+            set2->nodeTab[index2] = node1;
+        }
+
+        xmlXPathNodeSetClear(set2, 1);
     } else {
 	int i, j, initNbSet1;
 	xmlNodePtr n1, n2;
@@ -3268,28 +3259,32 @@ xmlXPathNodeSetMergeAndClear(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
 	initNbSet1 = set1->nodeNr;
 	for (i = 0;i < set2->nodeNr;i++) {
 	    n2 = set2->nodeTab[i];
-	    /*
-	    * Skip duplicates.
-	    */
-	    for (j = 0; j < initNbSet1; j++) {
-		n1 = set1->nodeTab[j];
-		if (n1 == n2) {
-		    goto skip_node;
-		} else if ((n1->type == XML_NAMESPACE_DECL) &&
-		    (n2->type == XML_NAMESPACE_DECL))
-		{
-		    if ((((xmlNsPtr) n1)->next == ((xmlNsPtr) n2)->next) &&
-			(xmlStrEqual(((xmlNsPtr) n1)->prefix,
-			((xmlNsPtr) n2)->prefix)))
-		    {
-			/*
-			* Free the namespace node.
-			*/
-			xmlXPathNodeSetFreeNs((xmlNsPtr) n2);
-			goto skip_node;
-		    }
-		}
-	    }
+
+            if (checkDupls) {
+                /*
+                * Skip duplicates.
+                */
+                for (j = 0; j < initNbSet1; j++) {
+                    n1 = set1->nodeTab[j];
+                    if (n1 == n2) {
+                        goto skip_node;
+                    } else if ((n1->type == XML_NAMESPACE_DECL) &&
+                        (n2->type == XML_NAMESPACE_DECL))
+                    {
+                        if ((((xmlNsPtr) n1)->next == ((xmlNsPtr) n2)->next) &&
+                            (xmlStrEqual(((xmlNsPtr) n1)->prefix,
+                            ((xmlNsPtr) n2)->prefix)))
+                        {
+                            /*
+                            * Free the namespace node.
+                            */
+                            xmlXPathNodeSetFreeNs((xmlNsPtr) n2);
+                            goto skip_node;
+                        }
+                    }
+                }
+            }
+
 	    /*
 	    * grow the nodeTab if needed
 	    */
@@ -3301,50 +3296,10 @@ xmlXPathNodeSetMergeAndClear(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
 skip_node:
             set2->nodeTab[i] = NULL;
 	}
+
+        set2->nodeNr = 0;
     }
 
-    set2->nodeNr = 0;
-    return(set1);
-
-error:
-    xmlXPathFreeNodeSet(set1);
-    xmlXPathNodeSetClear(set2, 1);
-    return(NULL);
-}
-
-/**
- * xmlXPathNodeSetMergeAndClearNoDupls:
- * @set1:  the first NodeSet or NULL
- * @set2:  the second NodeSet
- *
- * Merges two nodesets, all nodes from @set2 are added to @set1.
- * Doesn't check for duplicate nodes. Clears set2.
- *
- * Returns @set1 once extended or NULL in case of error.
- *
- * Frees @set1 in case of error.
- */
-static xmlNodeSetPtr
-xmlXPathNodeSetMergeAndClearNoDupls(xmlNodeSetPtr set1, xmlNodeSetPtr set2,
-                                    xmlXPathEvalMode mode) {
-    if ((mode == XPATH_EVAL_FIRST) || (mode == XPATH_EVAL_LAST)) {
-        if (xmlXPathNodeSetMergeFirstLast(set1, set2, mode) < 0)
-            goto error;
-    } else {
-	int i;
-	xmlNodePtr n2;
-
-	for (i = 0;i < set2->nodeNr;i++) {
-	    n2 = set2->nodeTab[i];
-            if (set1->nodeNr >= set1->nodeMax) {
-                if (xmlXPathNodeSetGrow(set1) < 0)
-                    goto error;
-            }
-	    set1->nodeTab[set1->nodeNr++] = n2;
-            set2->nodeTab[i] = NULL;
-	}
-    }
-    set2->nodeNr = 0;
     return(set1);
 
 error:
@@ -11083,8 +11038,8 @@ xmlXPathNodeCollectAndTest(xmlXPathParserContextPtr ctxt,
     int hasPredicateRange, hasAxisRange, pos;
     int breakOnFirstHit;
     int direction;
+    int checkDupls;
 
-    xmlXPathNodeSetMergeFunction mergeAndClear;
     xmlNodePtr oldContextNode;
     xmlXPathContextPtr xpctxt = ctxt->context;
 
@@ -11110,15 +11065,15 @@ xmlXPathNodeCollectAndTest(xmlXPathParserContextPtr ctxt,
     /*
     * Setup axis.
     */
-    mergeAndClear = xmlXPathNodeSetMergeAndClear;
     switch (axis) {
         case AXIS_ATTRIBUTE:
         case AXIS_CHILD:
         case AXIS_NAMESPACE:
         case AXIS_SELF:
-	    mergeAndClear = xmlXPathNodeSetMergeAndClearNoDupls;
+            checkDupls = 0;
             break;
         default:
+            checkDupls = 1;
             break;
     }
 
@@ -11385,7 +11340,8 @@ no_match:
 		outSeq = seq;
 		seq = NULL;
 	    } else {
-		outSeq = mergeAndClear(outSeq, seq, mode);
+		outSeq = xmlXPathNodeSetMergeAndClear(outSeq, seq, mode,
+                                                      checkDupls);
                 if (outSeq == NULL)
                     xmlXPathPErrMemory(ctxt);
 	    }
@@ -11675,7 +11631,8 @@ xmlXPathCompOpEval(xmlXPathParserContextPtr ctxt, xmlXPathEvalMode mode,
                     arg2->nodesetval = tmp;
                 } else {
                     arg1->nodesetval = xmlXPathNodeSetMergeAndClear(
-                            arg1->nodesetval, arg2->nodesetval, mode);
+                            arg1->nodesetval, arg2->nodesetval, mode,
+                            /* checkDupls */ 1);
                     if (arg1->nodesetval == NULL)
                         xmlXPathPErrMemory(ctxt);
                 }
