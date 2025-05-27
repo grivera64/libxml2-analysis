a5b749465ccab2dc5fa8d5c3ce25b65c61eb42f5
Giovanni Rivera <rivera.giovanni271@gmail.com>
2025-05-27 03:19:32 -0700

    ♻ Refactor: Add testlimits.c as an entrypoint

--- run_analysis.sh
+++ run_analysis.sh
@@ -6,10 +6,21 @@ fi
 
 # Extract bitcode for xmlmemory
 extract-bc xmlmemory.o
+extract-bc error.o
+extract-bc parser.o
+extract-bc threads.o
+extract-bc testlimits.o
+# extract-bc testReader.o
+
+llvm-link xmlmemory.o.bc error.o.bc parser.o.bc threads.o.bc testlimits.o.bc -o testlimits.bc
+# llvm-link xmlmemory.o.bc error.o.bc parser.o.bc threads.o.bc testReader.o.bc -o testReader.bc
 
 # Convert to readable LLVM IR (just for human analysis)
-llvm-dis xmlmemory.o.bc
+llvm-dis testlimits.bc
+# llvm-dis testReader.bc
+
 
 # Analyze the bitcode using the unified pass
-opt -load $UNIFIED_PATH -unified xmlmemory.o.bc -disable-output 2>&1 | tee xmlmemory.txt
+opt -load $UNIFIED_PATH -unified testlimits.bc -disable-output 2>&1 | tee testlimits.txt
+# opt -load $UNIFIED_PATH -unified testReader.bc -disable-output 2>&1 | tee testReader.txt
 
